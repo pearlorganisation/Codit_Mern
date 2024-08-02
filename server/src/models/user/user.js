20206dcc622f2f624af70bc3a5400e54fda91e2d
nayan-kunwar26 <nayan@pearlorganisation.com>
2024-08-02 15:38:13 +0530

    configure tailwind css

--- server/src/models/user/user.js
+++ server/src/models/user/user.js
@@ -2,41 +2,18 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 
-// Address Schema
-const addressSchema = new mongoose.Schema({
-  street: {
-    type: String,
-    required: [true, "Street is required"],
-    trim: true,
-  },
-  city: {
-    type: String,
-    required: [true, "City is required"],
-    trim: true,
-  },
-  state: {
-    type: String,
-    required: [true, "State is required"],
-    trim: true,
-  },
-  zip: {
-    type: String,
-    required: [true, "Zip is required"],
-    trim: true,
-  },
-  country: {
-    type: String,
-    required: [true, "Country is required"],
-    trim: true,
-  },
-});
 
 // User Schema
 const userSchema = new mongoose.Schema(
   {
-    name: {
+    firstName: {
       type: String,
-      required: [true, "Name is required"],
+      required: [true, "First name is required"],
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"],
       trim: true,
     },
     email: {
@@ -52,9 +29,9 @@ const userSchema = new mongoose.Schema(
       minlength: 6,
       trim: true,
     },
-    mobileNumber: {
+    phoneNumber: {
       type: String,
-      required: [true, "Mobile number is required"],
+      required: [true, "Phone number is required"],
       trim: true,
     },
     addresses: [addressSchema],
