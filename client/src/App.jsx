0cb9ad85600f8233fc352e83142d9a4c81a1fa02
nayan-kunwar26 <nayan@pearlorganisation.com>
2024-08-05 12:41:26 +0530

    Created swiper for collection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Collections } from "./components/Collections/Collections";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-    </>
-  );
+  return <Collections />;
 }
 
 export default App;
