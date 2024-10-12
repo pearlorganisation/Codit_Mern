25be29ccf34264acd0a25ebc4c04c97f1c11d4e5
Ranjan <ranjan@pearlorganisation.com>
2024-10-12 16:45:48 +0530

    minor addition in credit card modal

--- client/src/components/CreditCard/CreditCard.jsx
+++ client/src/components/CreditCard/CreditCard.jsx
@@ -19,6 +19,17 @@ const CreditCard= ({ handleCredit }) => {
                     </div>
                     <div className="mb-4">
                         <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                            Expiry Date 
+                        </label>
+                        <input
+                            type="date"
+                            id="card-expiry"
+                            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm h-12"
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="name" className="block text-sm font-medium text-gray-700">
                             Card Company
                         </label>
                         <input
